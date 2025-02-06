e29f25fe5c6466c85b95b4ba920358c77d346686
Aravind <Aravindratnam1996@gmail.com>
2025-02-06 17:57:50 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// Example of using Typed.js for dynamic text
+const options = {
+  strings: ["Web Developer", "UI/UX Designer","Frontend Developer", "Creative Thinker","Full Stack developer"],
+  typeSpeed: 20,
+  backSpeed: 20,
+  loop: true
+};
+
+const typed = new Typed('.text', options);
